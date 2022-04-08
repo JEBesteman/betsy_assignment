abf6e4544b61867c4dd2985f0cd947936507f0d4
Jolanda Besteman <jebesteman@gmail.com>
2022-04-08 16:00:34 +0200

    changed main.py after feedback David

--- betsy-webshop/main.py
+++ betsy-webshop/main.py
@@ -2,22 +2,20 @@ __winc_id__ = "d7b474e9b3a54d23bca54879a4f1855b"
 __human_name__ = "Betsy Webshop"
 
 
+from math import prod
 from models import *
 
 # search for products based on a term, case insensitive!
 def search(term: str):
     search_term = term.lower()
-    match = Product.select().where(
-        (fn.LOWER(Product.name).contains(search_term))
-        | (fn.LOWER(Product.description).contains(search_term))
-    )
-    if match:
-        print(f"There is a match for term {term}:")
-        for product in match:
-            print(product.name)
-            return product
-    else:
-        print("Sorry, no product is found")
+    products = Product.select()
+    return [
+        product.name
+        for product in products.where(
+            (fn.LOWER(Product.name).contains(search_term))
+            | (fn.LOWER(Product.description).contains(search_term))
+        )
+    ]
 
 
 # view products of a given user
@@ -25,30 +23,28 @@ def list_user_products(user_id: int):
     products_list = (
         Product.select().join(UserProduct).join(User).where(User.id == user_id)
     )
-    if products_list:
-        print(f"User id {user_id} has:")
-        for product in products_list:
-            print(product.name)
-    else:
-        print("This user is not found or has no products to sell")
-    return products_list
+    return [product.name for product in products_list]
 
 
 # view all products for a given tag
-def list_products_per_tag(tag_id: int):
-    products_list = Product.select().join(ProductTag).join(Tag).where(Tag.id == tag_id)
-    if products_list:
-        print(f"These are the products that are tagged with tag id {tag_id}:")
-        for product in products_list:
-            print(product.name)
-    else:
-        print("This tag does not exist or there are no products with that tag")
-    return products_list
+# def list_products_per_tag(tag_id: int):
+#     products_list = Product.select().join(ProductTag).join(Tag).where(Tag.id == tag_id)
+#     return [product.name for product in products_list]
+
+# nu met "sports" als tag_id
+def list_products_per_tag(tag_id: str):
+    products_list = (
+        Product.select().join(ProductTag).join(Tag).where(Tag.name == tag_id)
+    )
+    return [product.name for product in products_list]
 
 
 # I have chosen for the product_id as parameter, to add a product to user
 def add_product_to_catalog(user_id: int, product_id):
-    UserProduct.create(user=user_id, product=product_id, quantity=1)
+    try:
+        UserProduct.create(user=user_id, product=product_id, quantity=1)
+    except:
+        return None
 
 
 """ Update the stock quantity of a product
@@ -58,10 +54,13 @@ I have chosen to update Product (database) en not UserProduct.
 
 
 def update_stock(product_id: int, new_quantity: int):
-    update = Product.update({Product.quantity: new_quantity}).where(
-        Product.id == product_id
-    )
-    update.execute()
+    try:
+        update = Product.update({Product.quantity: new_quantity}).where(
+            Product.id == product_id
+        )
+        update.execute()
+    except:
+        return None
 
 
 """Handle purchase between buyer and sell for a given product
@@ -71,13 +70,16 @@ and update the stock of the database
 
 
 def purchase_product(product_id: int, buyer_id: int, quantity: int = 1):
-    product_to_buy = Product.get_by_id(product_id)
-    available_amount = product_to_buy.quantity - quantity
-    if available_amount >= 0:
-        Transaction.create(buyer=buyer_id, product=product_id, quantity=quantity)
-        print("You're transaction is completed!")
-    # update stock
-    update_stock(product_id, available_amount)
+    try:
+        product_to_buy = Product.get_by_id(product_id)
+        available_amount = product_to_buy.quantity - quantity
+        if available_amount >= 0:
+            Transaction.create(buyer=buyer_id, product=product_id, quantity=quantity)
+            print("You're transaction is completed!")
+            # update stock
+            update_stock(product_id, available_amount)
+    except:
+        return None
 
 
 """Remove a product from an user
@@ -93,3 +95,4 @@ def remove_product(product_id: int):
         product.delete_instance(recursive=True)
     except DoesNotExist:
         print("No product with this id has been found")
+        return None
